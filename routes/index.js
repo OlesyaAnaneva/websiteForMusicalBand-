6d4c0000b943d7f1d4cc7fe750464a88046bebfd
Olesya <ananeva.olesya@yandex.ru>
2020-05-22 17:01:48 +0300

    final

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,29 @@
 const router = require("express").Router();
 const contactModel = require('../models/contactModel');
 
-router.get("/", async (req, res) => {
-  const contactdata = await contactModel.find().update();
-  console.log(contactdata);
-  
-  res.render("index", {  });
+router.get("/",  (req, res) => {
+  // try {
+  //   const contactdata = await contactModel.find();
+  //   console.log(contactdata);
+  // } catch (err) {
+  //   console.log(err)
+  // }
+  res.render("index");
 });
 
-router.get("/contactBack", (req, res) => {
-  res.render("back/contactBack");
-});
+// router.get("/contactBack", (req, res) => {
+//   res.render("back/contactBack");
+// });
 
-router.post("/contactBack", async (req, res) => {
-  const contactdata = await contactModel.create({
-    job: req.body.job,
-    nameJob: req.body.nameJob,
-    emailJob: req.body.emailJob,
-    telJob: req.body.telJob,
-  });
-  res.redirect('/');
-});
+// router.post("/contactBack", async (req, res) => {
+//   await contactModel.remove();
+//   await contactModel.create({
+//     job: req.body.job,
+//     nameJob: req.body.nameJob,
+//     emailJob: req.body.emailJob,
+//     telJob: req.body.telJob,
+//   });
+//   res.redirect('/');
+// });
 
 module.exports = router;
